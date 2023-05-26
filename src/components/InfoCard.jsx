c2098e45e8ac1189f414855803fb8fcc1a75fd1d
prakhar <autogear070@gmail.com>
2023-05-26 11:17:54 +0530

    first commit

---
+++ src/components/InfoCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const isIncome = Math.round(Math.random());
+
+const InfoCard = () => {
+  return (
+    <div elevation={3} style={{ textAlign: 'center', padding: '0 10%' }}>
+      Add your expense here 😃
+    </div>
+  );
+};
+
+export default InfoCard;
